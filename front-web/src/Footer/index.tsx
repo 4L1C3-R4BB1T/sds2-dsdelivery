bddd1b42d78595f5b9a4160b904a2172b9a71ebd
Livia <livinha.guimaraes.013@gmail.com>
2021-01-08 12:02:04 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
