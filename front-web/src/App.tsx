a557812a138b4f3aade2e9a3d273b30020e84b89
Livia <livinha.guimaraes.013@gmail.com>
2021-01-08 11:04:37 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
