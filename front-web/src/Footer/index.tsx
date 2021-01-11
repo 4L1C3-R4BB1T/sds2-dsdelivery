57ad6c2792628e547d4afd8650bd9ef8e6841983
Livia <livinha.guimaraes.013@gmail.com>
2021-01-11 16:31:26 -0300

    Add routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,7 +1,7 @@
-import './styles.css';
-import { ReactComponent as InstagramIcon } from './instagram.svg';
-import { ReactComponent as LinkedinIcon } from './linkedin.svg';
-import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import "./styles.css";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
+import { ReactComponent as LinkedinIcon } from "./linkedin.svg";
+import { ReactComponent as YoutubeIcon } from "./youtube.svg";
 
 function Footer() {
     return (
@@ -14,7 +14,7 @@ function Footer() {
                 <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
                     <LinkedinIcon />
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig/">
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
                     <InstagramIcon />
                 </a>
             </div>
