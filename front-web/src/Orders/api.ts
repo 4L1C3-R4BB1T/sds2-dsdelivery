6a603dd5fd4970e0eacd26fcbee710e8185b6b09
Livia <livinha.guimaraes.013@gmail.com>
2021-01-22 23:29:13 -0300

    Api link update and add images

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://sds2-backend.herokuapp.com/'
+const API_URL = 'https://backend-sds2.herokuapp.com'
 
 export function fetchProducts() {
     return axios(`${API_URL}/products`);
