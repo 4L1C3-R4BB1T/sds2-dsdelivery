e338fee18286f8d449fd632b3d01c93b24107271
Livia <livinha.guimaraes.013@gmail.com>
2021-02-06 13:54:32 -0300

    Add orders list

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import Header from '../Header';
+import OrderCard from '../OrderCard';
+
+export default function Orders() {
+    return (
+        <>
+            <Header />
+            <ScrollView style={styles.container}>
+                <OrderCard />
+            </ScrollView>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        paddingRight: '5%',
+        paddingLeft: '5%'
+    }
+});
